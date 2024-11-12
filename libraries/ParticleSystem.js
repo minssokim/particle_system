6efa41d5adfd1c302326b65fbbd5f71882281e72
minssokim <minssokim6@gmail.com>
2024-11-12 15:38:18 +0900

    중력을 시스템에 적용

--- libraries/ParticleSystem.js
+++ libraries/ParticleSystem.js
@@ -6,7 +6,13 @@ class ParticleSystem {
     addParticle(){
         this.particles.push(new Particle(this.origin));
     }
-    run() {
+
+    applyGravity(g) {
+        for (let p of this.particles){
+            p.applyForce(g);
+        }
+}    
+run() {
         for (let i = this.particles.length-1; i >= 0; i--) {
             let p = this.particles[i];
             p.run();
