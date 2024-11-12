e465448f8a221f13ef3c1dcc93b13483671f9b70
minssokim <minssokim6@gmail.com>
2024-11-12 15:24:12 +0900

    리펙토링

---
+++ libraries/Particle.js
@@ -0,0 +1,70 @@
+
+class Particle {
+    constructor(position) {
+        this.acceleration = createVector(0, 0.05);
+        this.velocity = createVector(random(-1, 1), random(-1, 0));
+        this.position = position.copy();
+        this.lifespan = 255;
+    }
+
+    run() {
+        this.update();
+        this.display();
+    }
+
+    update() {
+        this.velocity.add(this.acceleration);
+        this.position.add(this.velocity);
+        this.lifespan -= 2;
+    }
+
+    display() {
+        stroke(200, this.lifespan);
+        strokeWeight(2);
+        fill(127, this.lifespan);
+        ellipse(this.position.x, this.position.y, 12, 12);
+    }
+
+    isDead() {
+        return this.lifespan < 0;
+    }
+}
+
+
+
+
+
+
+
+/*let Particle = function(position) {
+    this.acceleration = createVector(0, 0.05);
+    this.velocity = createVector(random(-1, 1), random(-1, 0));
+    this.position = position.copy();
+    this.lifespan = 255;
+  };
+  
+  Particle.prototype.run = function() {
+    this.update();
+    this.display();
+  };
+  
+  // Method to update position
+  Particle.prototype.update = function(){
+    this.velocity.add(this.acceleration);
+    this.position.add(this.velocity);
+    this.lifespan -= 2;
+  };
+  
+  // Method to display
+  Particle.prototype.display = function() {
+    stroke(200, this.lifespan);
+    strokeWeight(2);
+    fill(127, this.lifespan);
+    ellipse(this.position.x, this.position.y, 12, 12);
+  };
+  
+  // Is the particle still useful?
+  Particle.prototype.isDead = function(){
+    return this.lifespan < 0;
+  };
+  */
